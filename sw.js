706ecc6e78aff9b04b261faeb0bb14caef36d0b6
Abhi.Patel <Abhi.Patel@kuehne-nagel.com>
2026-07-29 12:21:40 -0400

    update

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'scout-v1';
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['./', './index.html'])).catch(() => {}));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => cached))
+  );
+});
